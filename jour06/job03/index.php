b9605ed7f3819c6cc201f5be283b73fc316ae449
Maiilaa <zirarimaila8@gmail.com>
2024-09-24 16:59:19 +0200

    aa

---
+++ jour06/job03/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Layout</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 box box-1"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 col-sm-12 box box-2"></div>
+            <div class="col-md-4 col-sm-12 box box-2"></div>
+            <div class="col-md-4 col-sm-12 box box-2"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6 col-sm-12 box box-3"></div>
+            <div class="col-md-6 col-sm-12 box box-3"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 col-sm-12 box box-4"></div>
+            <div class="col-md-4 col-sm-12 box box-4"></div>
+            <div class="col-md-4 col-sm-12 box box-4"></div>
+        </div>
+    </div>
+
+</body>
+</html>
